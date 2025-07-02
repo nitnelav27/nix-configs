b2393cf48d00c65dff88605cad126b386964c030
Valentin Vergara Hidd <valentinvergara@gmail.com>
2025-07-01 20:59:26 -0400

    adding main branch to dockmedia

--- hosts/dockmedia/modules/services.nix
+++ hosts/dockmedia/modules/services.nix
@@ -29,21 +29,21 @@
                         mountdPort = 4002;
                         statdPort = 4000;
                         exports = ''
-                                /export 10.27.81.115/32(insecure,rw,no_subtree_check,fsid=0) 10.27.81.81/32(insecure,rw,sync,all_squash,anonuid=1000,anongid=1000,no_subtree_check,fsid=0)
+                                /export 10.27.81.115/32(insecure,rw,no_subtree_check,fsid=0) 10.27.81.81/32(insecure,rw,sync,all_squash,anonuid=1000,anongid=1000,no_subtree_check,fsid=0) 10.27.81.82/32(insecure,rw,sync,all_squash,anonuid=1000,anongid=1000,no_subtree_check,fsid=0)
                                 ## decreto
-                                /export/.decreto 10.27.81.115/32(insecure,rw,no_subtree_check) 10.27.81.81/32(insecure,rw,sync,all_squash,anonuid=1000,anongid=1000,no_subtree_check)
+                                /export/.decreto 10.27.81.115/32(insecure,rw,no_subtree_check) 10.27.81.81/32(insecure,rw,sync,all_squash,anonuid=1000,anongid=1000,no_subtree_check) 10.27.81.82/32(insecure,rw,sync,all_squash,anonuid=1000,anongid=1000,no_subtree_check)
                                 ## calibre
-                                /export/calibre 10.27.81.115/32(insecure,rw,no_subtree_check) 10.27.81.81/32(insecure,rw,sync,all_squash,anonuid=1000,anongid=1000,no_subtree_check)
+                                /export/calibre 10.27.81.115/32(insecure,rw,no_subtree_check) 10.27.81.81/32(insecure,rw,sync,all_squash,anonuid=1000,anongid=1000,no_subtree_check) 10.27.81.82/32(insecure,rw,sync,all_squash,anonuid=1000,anongid=1000,no_subtree_check)
                                 ## data
-                                /export/data 10.27.81.115/32(insecure,rw,no_subtree_check) 10.27.81.81/32(insecure,rw,sync,all_squash,anonuid=1000,anongid=1000,no_subtree_check)
+                                /export/data 10.27.81.115/32(insecure,rw,no_subtree_check) 10.27.81.81/32(insecure,rw,sync,all_squash,anonuid=1000,anongid=1000,no_subtree_check) 10.27.81.82/32(insecure,rw,sync,all_squash,anonuid=1000,anongid=1000,no_subtree_check)
                                 ## dump
-                                /export/dump 10.27.81.115/32(insecure,rw,no_subtree_check) 10.27.81.81/32(insecure,rw,sync,all_squash,anonuid=1000,anongid=1000,no_subtree_check)
+                                /export/dump 10.27.81.115/32(insecure,rw,no_subtree_check) 10.27.81.81/32(insecure,rw,sync,all_squash,anonuid=1000,anongid=1000,no_subtree_check) 10.27.81.82/32(insecure,rw,sync,all_squash,anonuid=1000,anongid=1000,no_subtree_check)
                                 ## migration
                                 /export/migration 10.27.81.115/32(insecure,rw,no_subtree_check)
                                 ## docs
-                                /export/docs 10.27.81.115/32(insecure,rw,no_subtree_check) 10.27.81.81/32(insecure,rw,sync,all_squash,anonuid=1000,anongid=1000,no_subtree_check)
+                                /export/docs 10.27.81.115/32(insecure,rw,no_subtree_check) 10.27.81.81/32(insecure,rw,sync,all_squash,anonuid=1000,anongid=1000,no_subtree_check) 10.27.81.82/32(insecure,rw,sync,all_squash,anonuid=1000,anongid=1000,no_subtree_check)
                                 ## results
-                                /export/results 10.27.81.115/32(insecure,rw,no_subtree_check) 10.27.81.81/32(insecure,rw,sync,all_squash,anonuid=1000,anongid=1000,no_subtree_check)
+                                /export/results 10.27.81.115/32(insecure,rw,no_subtree_check) 10.27.81.81/32(insecure,rw,sync,all_squash,anonuid=1000,anongid=1000,no_subtree_check) 10.27.81.82/32(insecure,rw,sync,all_squash,anonuid=1000,anongid=1000,no_subtree_check)
                         '';
                 };
         };
