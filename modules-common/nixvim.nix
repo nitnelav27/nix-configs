8b64131b957f8396b3babddbd1799eefae2a9ace
Valentin Vergara Hidd <valentinvergara@gmail.com>
2025-08-13 22:52:52 -0400

    started making progress with nixvim

--- modules-common/nixvim.nix
+++ modules-common/nixvim.nix
@@ -3,24 +3,71 @@
 {
   programs.nixvim = {
     enable = true;
-
-    # Set global options
-      #expandtab = true;
-
-    # Clipboard and theme
-    clipboard = {
-      providers = { wl-copy = { enable = true; }; };
-      register = "unnamedplus";
+    defaultEditor = true;
+    viAlias = true;
+    enableMan = true;
+    opts = {
+	number = true;
+      	relativenumber = false;
+	expandtab = true;
+	ignorecase = true;
+	smartcase = true;
+	hlsearch = true;
+	cursorline = true;
+	visualbell = true;
+        mouse = "a";
+        showmode = false;
+        clipboard = {
+            providers = {
+                wl-copy.enable = true;
+            };
+            register = "unnamedplus";
+        };
     };
 
-   colorschemes.nord.enable = true; 
+    ## Performance
+    performance = {
+      byteCompileLua = {
+        enable = true;
+        nvimRuntime = true;
+        configs = true;
+        plugins = true;
+      };
+    };
+
+
+   colorschemes = {
+        nord = {
+            enable = true;
+            settings = {
+                borders = true;
+                contrast = true;
+                disable_background = true;
+            };
+        };
+   };
 
     # Plugins
     plugins = {
+        web-devicons.enable = true;
+        rainbow-delimiters.enable = true;
+        nix.enable = true;
+        nix-develop.enable = true;
+        cmp = {
+          autoLoad = true;
+          autoEnableSources = true;
+          settings = {
+            sources = [
+              { name = "nvim_lsp"; }
+              { name = "path"; }
+              { name = "buffer"; }
+            ];
+          };
+        };
       nvim-autopairs.enable = true;
       #obsidian-nvim
       colorizer.enable = true;
-      blink-cmp.enable = true;
+      #blink-cmp.enable = true;
       #catppuccin.enable = true;
       conform-nvim.enable = true;
       dressing.enable = true;
@@ -43,7 +90,7 @@
       trouble.enable = true;
       which-key.enable = true;
       vimtex.enable = true;
-      cmp-vimtex.enable = true;
+      #cmp-vimtex.enable = true;
       notify.enable = true;
       treesitter.enable = true;
       dap-python.enable = true;
