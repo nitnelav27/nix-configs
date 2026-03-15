f030a1305a6511f0d742a9eb7b9ad21c0ef3db55
Valentin Vergara Hidd <valentinvergara@gmail.com>
2026-03-15 00:29:16 -0300

    fixed qbittorrent

--- modules/media-services/qbittorrent.nix
+++ modules/media-services/qbittorrent.nix
@@ -1,13 +1,21 @@
 { config, lib, pkgs, inputs, ... }:
 
 {
-    services.qbittorrent = {
+  services.qbittorrent = {
         enable = true;
         package = pkgs.qbittorrent-nox;
         user = "vvh";
         group = "vvh";
         openFirewall = true;
-        profileDir = "/home/vvh/appData/qBittorrent";
+        profileDir = "/home/vvh/appData";
         webuiPort = 8080;
+        extraArgs = [ "--confirm-legal-notice" ];
+    };
+
+    systemd.services.qbittorrent.serviceConfig = {
+        # Use mkForce to override the default "yes" from the NixOS module
+        ProtectHome = lib.mkForce false;
+        WorkingDirectory = "/home/vvh/appData/qBittorrent";
+        Environment = "QT_QPA_PLATFORM=offscreen";
     };
 }
