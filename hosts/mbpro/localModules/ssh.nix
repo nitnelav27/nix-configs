3e6a5f85b76de18f692218202a7ce353cce6c232
Valentin Vergara Hidd <valentinvergara@gmail.com>
2026-04-07 19:51:26 -0400

    updated flakes

---
+++ hosts/mbpro/localModules/ssh.nix
@@ -0,0 +1,35 @@
+{ config, lib, pkgs, inputs, ... }:
+
+{
+    programs.ssh = {
+        enable = true;
+        addKeysToAgent = "yes";
+        controlMaster = "auto";
+        controlPersist = "10m";        
+        matchBlocks = {
+            "*" = {
+                setEnv = {
+                    TERM = "xterm-256color";
+                };
+            };
+            "github.com" = {
+                port = 22;
+                addKeysToAgent = "yes";
+                identityFile = "~/.ssh/id_ed25519";
+            };
+            "media-ccp" = {
+                hostname = "10.27.115.4";
+                port = 1186;
+                user = "vvh";
+                identityFile = "~/.ssh/id_ed25519";
+            };
+            "rpi-ccp" = {
+                hostname = "10.27.115.3";
+                port = 1186;
+                user = "vvh";
+                identityFile = "~/.ssh/id_ed25519";
+            };
+        };
+    };
+
+}
